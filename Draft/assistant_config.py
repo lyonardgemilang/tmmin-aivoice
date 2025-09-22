5a5692748a38b3acfa2ab3a753852e561ffb36c4
lyonardgemilang <lyonardgemilang@student.ub.ac.id>
2025-09-22 08:19:34 +0700

    Push draft & clean code

---
+++ Draft/assistant_config.py
@@ -0,0 +1,14 @@
+﻿from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class AssistantConfig:
+    use_hailo_encoder: bool = True
+    hailo_encoder_hef: str = "base-whisper-encoder-5s_h8l.hef"
+    hailo_window_seconds: int = 5
+    use_google_stt: bool = False
+    decoder_local_dir: Optional[str] = None
+    send_to_webserver: bool = True
+    web_server_url: str = "http://10.0.0.76:5000/task"
+    use_esp: bool = True
